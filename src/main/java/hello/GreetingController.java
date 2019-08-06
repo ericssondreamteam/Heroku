f8a9c9f297bdd38be6e41b54f93352104975eae0
Kar0lLasek <19karolasek98@gmail.com>
2019-08-06 11:09:15 +0200

    First Commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String getHome(){return "home";}
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+
+}

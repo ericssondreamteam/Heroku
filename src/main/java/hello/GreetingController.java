eec7b7aa07fe97b95fd710bd352ff550a413433f
Kar0lLasek <19karolasek98@gmail.com>
2019-08-07 15:43:05 +0200

    Remove unused functions from Controller.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,25 +1,14 @@
 package hello;
 
-import org.springframework.mail.javamail.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.mail.*;
-import javax.mail.internet.*;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.*;
 
 @Controller
 public class GreetingController {
-
-    /*static String emailToRecipient, emailSubject, emailMessage;
-    static final String emailFromRecipient = "ericssonStart@outlook.com";
-
-
-    public JavaMailSender mailSender;*/
 
     @GetMapping("/")
     public String getHome() {
@@ -46,60 +35,6 @@ public class GreetingController {
     public String getSuccess() {
         return "success";
     }
-
-    /*JavaMailSender mailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.office365.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("ericssonStart@outlook.com");
-        mailSender.setPassword("qwerty12345");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//add
-        props.put("mail.smtp.socketFactory.port", "465");                           //add
-        props.put("mail.smtp.starttls.enable", "true");                             //add
-        props.put("mail.smtp.quitwait", "false");
-        mailSender.setJavaMailProperties(props);
-
-        return mailSender;
-    }*/
-
-    //OLD FUNCTION
-
-    /*@RequestMapping(value = "sendEmail", method = RequestMethod.POST)
-    public String sendEmailToClient(HttpServletRequest request) {
-
-        mailSender = mailSender();
-
-        // Reading Email Form Input Parameters
-        emailSubject = request.getParameter("subject");
-        emailMessage = request.getParameter("message");
-        emailToRecipient = request.getParameter("mailTo");
-
-        // Logging The Email Form Parameters For Debugging Purpose
-        System.out.println("\nReceipient?= " + emailToRecipient + ", Subject?= " + emailSubject + ", Message?= " + emailMessage + "\n");
-
-        try {
-            mailSender.send(new MimeMessagePreparator() {
-                public void prepare(MimeMessage mimeMessage) throws Exception {
-
-                    MimeMessageHelper mimeMsgHelperObj = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-                    mimeMsgHelperObj.setTo(emailToRecipient);
-                    mimeMsgHelperObj.setFrom(emailFromRecipient);
-                    mimeMsgHelperObj.setText(emailMessage);
-                    mimeMsgHelperObj.setSubject(emailSubject);
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return "success";
-    }*/
-
 
     //NEW VERSION (fast as fuck)XD
     @RequestMapping(value = "sendEmail", method = RequestMethod.POST)
